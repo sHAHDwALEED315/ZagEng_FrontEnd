99ab4bf4718491ccc23dd4df64326e16972d63df
Shahd Waleed <shahdwaleed20017@gmail.com>
2026-06-14 21:36:03 +0300

    done

--- React_Challenges/React-challeng-2/src/ProductCards.jsx
+++ React_Challenges/React-challeng-2/src/ProductCards.jsx
@@ -2,7 +2,21 @@ import Card from "./Card"
 import useFetch from "./useFetch";
 
 const ProductCards = () => {
+
   const productstyle = { display: "flex"};
+
+  const { data, loading, error } = useFetch(
+    "https://fakestoreapi.com/products"
+  );
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error fetching data</h2>;
+  }
+  
   return (
     <div style={productstyle}>
       {data.map((product) => (
